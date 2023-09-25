1b91592bc411ee9d1d2e27a722ed1c26185c4f55
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-09-25 23:15:51 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function showNavbar() {
+    var x = document.getElementById("menu-icon");
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
